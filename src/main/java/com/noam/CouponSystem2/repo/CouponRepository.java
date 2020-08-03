7ddafc55dbcb2eee4618170c743f0b48e420c669
Noam Katzburg <noamkatzburg@gmail.com>
2020-08-03 20:51:03 +0300

    fixed expicit query

--- src/main/java/com/noam/CouponSystem2/repo/CouponRepository.java
+++ src/main/java/com/noam/CouponSystem2/repo/CouponRepository.java
@@ -1,5 +1,7 @@
 package com.noam.CouponSystem2.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,9 @@ import com.noam.CouponSystem2.beans.Coupon;
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
 
+	//get All Coupons By Company Id
+	List<Coupon> findByCompanyId(int id);
+	
+	
+	
 }
