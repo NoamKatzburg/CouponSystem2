a9dd26faa1007095be17f3b6ea592a0a206ec8e7
Noam Katzburg <noamkatzburg@gmail.com>
2020-08-04 20:21:38 +0300

    will use later

--- src/main/java/com/noam/CouponSystem2/controller/CompanyController.java
+++ src/main/java/com/noam/CouponSystem2/controller/CompanyController.java
@@ -1,18 +1,15 @@
 package com.noam.CouponSystem2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.noam.CouponSystem2.service.CompanyFacade;
+
 
 
 @RestController
 @RequestMapping("company")
 public class CompanyController {
 
-	@Autowired
-	private CompanyFacade service;
 	
 }
